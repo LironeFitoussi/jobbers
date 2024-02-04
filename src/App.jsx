bb7d32404df62216ec200a31614af23b429bac3b
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-02-04 10:33:48 +0200

    user porovider set to app

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Authentification from './pages/Authentication/Authentication.jsx'
 import Home from './pages/Authentication/Home.jsx'
 import Matches from './pages/Matches/Matches.jsx'
-
 function App() {
 
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path='/auth' element={< Authentification />} />
-        <Route path='/matches' element={< Matches />} />
-        <Route path='/settigns' element={< Settings />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <userProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/auth' element={< Authentification />} />
+          <Route path='/matches' element={< Matches />} />
+          <Route path='/settigns' element={< Settings />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </userProvider>
   )
 }
 
