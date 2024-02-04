d8988119b2665a8c918bab5f19e3b7716228cdfc
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-02-04 11:56:27 +0200

    User Card Created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,11 @@
+import UserCard from "../../components/UserCard/UserCard"
+
 export default function Home() {
     return (
         <section>
             <header>
                 <h1>This is <b>Home</b> Page</h1>
+                <UserCard />
             </header>
         </section>
     )
