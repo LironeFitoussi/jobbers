38a4146b1c89f4f4f0fa0541ba7f8be405125819
OfekMajar <ofek.majar@gmail.com>
2024-02-04 20:42:05 +0200

    showing cards on home

--- src/pages/Find/Find.jsx
+++ src/pages/Find/Find.jsx
@@ -1,19 +1,32 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Swiper from "../../components/Swiper/Swiper";
-import { UserContext } from '../../context/User'
-import styles from './Find.module.css'
-import { useParams } from "react-router-dom";
+import { UserContext } from "../../context/User";
+import styles from "./Find.module.css";
+import { useParams, useSearchParams } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../config/firebase";
 export default function Find() {
-    const { user } = useContext(UserContext)
-    
-    
-  
-    return (
-        <section>
-            <header>
-                <h1>This is <b>Home</b> Page</h1>
-                <Swiper userType={user.type} />
-            </header>
-        </section>
-    )
+  const [serviceData, SetServiceData] = useState();
+  const { id } = useParams();
+
+  const getServiceType = async () => {
+    const snapshot = await getDoc(doc(db, "Services", id));
+    console.log();
+    SetServiceData(snapshot.data());
+  };
+
+  useEffect(() => {
+    getServiceType();
+  }, [useParams]);
+
+  return (
+    <section>
+      <header className={styles.findPageHeader}>
+        <h1>
+          This is <b>Home</b> Page
+        </h1>
+        <Swiper userType={serviceData?.type} />
+      </header>
+    </section>
+  );
 }
