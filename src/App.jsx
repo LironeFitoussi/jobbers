4e891333d8704b96dfc890639169888b2778f2a5
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-02-04 10:31:41 +0200

    baisc routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Authentification from './pages/Authentication/Authentication.jsx'
+import Home from './pages/Authentication/Home.jsx'
+import Matches from './pages/Matches/Matches.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/auth' element={< Authentification />} />
+        <Route path='/matches' element={< Matches />} />
+        <Route path='/settigns' element={< Settings />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
